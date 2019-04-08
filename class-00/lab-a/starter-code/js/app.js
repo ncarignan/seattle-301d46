a858300bd5d13402e5d8e726ffb432233b6ff011
Nicholas Carignan <nick.carignan@sbcglobal.net>
2019-04-08 10:28:46 -0700

    made a mistake, thats ok

--- class-00/lab-a/starter-code/js/app.js
+++ class-00/lab-a/starter-code/js/app.js
@@ -2,7 +2,7 @@
 
 const names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
-const leftImage = document.getElementById('left');
+const leftImage = document.getElementById('right');
 const centerImage = document.getElementById('center');
 const rightImage = document.getElementById('right');
 
